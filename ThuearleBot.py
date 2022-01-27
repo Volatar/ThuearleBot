bbd0ea806530bc0d0349ea9f803a4e0887b9043c
Taylor Sanderson <sanderson.taylor@gmail.com>
2022-01-27 16:09:53 -0500

    Showing off servers

--- ThuearleBot.py
+++ ThuearleBot.py
@@ -7,11 +7,16 @@ def main():
     load_dotenv()
     TOKEN = os.getenv('DISCORD_TOKEN')
 
+
     client = discord.Client()
 
     @client.event
     async def on_ready():
         print(f'{client.user} has connected to Discord!')
+
+        print(f'{client.user} is connected to the following guilds:')
+        for guild in client.guilds:
+            print(f'{guild.name} (id: {guild.id})')
 
     client.run(TOKEN)
 
