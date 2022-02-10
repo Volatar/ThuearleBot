4fca2bde38659335e709d4e5e29e722e8cf009c8
Taylor Sanderson <sanderson.taylor@gmail.com>
2022-02-10 12:55:56 -0500

    First message response code (!wh40k)

--- ThuearleBot.py
+++ ThuearleBot.py
@@ -1,5 +1,6 @@
 import os
 import discord
+import random
 from dotenv import load_dotenv
 
 
@@ -7,8 +8,9 @@ def main():
     load_dotenv()
     TOKEN = os.getenv('DISCORD_TOKEN')
 
-
-    client = discord.Client()
+    intents = discord.Intents.default()
+    intents.members = True
+    client = discord.Client(intents=intents)
 
     @client.event
     async def on_ready():
@@ -17,6 +19,22 @@ def main():
         print(f'{client.user} is connected to the following guilds:')
         for guild in client.guilds:
             print(f'{guild.name} (id: {guild.id})')
+            members = '\n - '.join([member.name for member in guild.members])
+            print(f'Guild Members:\n - {members}')
+
+    @client.event
+    async def on_message(message):
+        if message.author == client.user:
+            return
+
+        wh40k_responses = [
+            'https://tenor.com/view/heresy-detected-war-hammer-40k-gif-9829547',
+            'https://tenor.com/view/40k-astartes-bolter-boltgun-dakka-gif-17604583',
+        ]
+
+        if message.content == '!wh40k':
+            response = random.choice(wh40k_responses)
+            await message.channel.send(response)
 
     client.run(TOKEN)
 
